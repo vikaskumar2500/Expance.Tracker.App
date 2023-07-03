2e03dd1474f385d95bf09dfb6c31081d7b9ed4c4
vikaskumar0205 <vikaskumar808425@gmail.com>
2023-07-03 13:24:44 +0530

    add LocationOfExpenditure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-undef */
+import ExpanceItem from "./components/ExpanceItem";
 
-function App() {
+const expances = [
+  {
+    title: "Food",
+    amount: 10,
+    LocationOfExpenditure: "Kolkata",
+  },
+
+  {
+    title: "Petrol",
+    amount: 100,
+    LocationOfExpenditure: "Delhi",
+  },
+
+  {
+    title: "Movies",
+    amount: 200,
+    LocationOfExpenditure: "Mumbai",
+  },
+];
+
+
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Expance Items</h2>
+      <ExpanceItem
+        title={expances[0].title}
+        amount={expances[0].amount}
+        LocationOfExpenditure={expances[0].LocationOfExpenditure}
+      />
+
+      <ExpanceItem
+        title={expances[1].title}
+        amount={expances[1].amount}
+        LocationOfExpenditure={expances[1].LocationOfExpenditure}
+      />
+
+      <ExpanceItem
+        title={expances[2].title}
+        amount={expances[2].amount}
+        LocationOfExpenditure={expances[2].LocationOfExpenditure}
+      />
     </div>
   );
 }
