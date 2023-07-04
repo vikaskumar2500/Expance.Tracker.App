acbb03c1f73cfa634f78e42b7851ef9d1f2e868b
vikaskumar0205 <vikaskumar808425@gmail.com>
2023-07-04 06:20:50 +0530

    updated css

--- src/components/subComponents/ExpanceDate.js
+++ src/components/subComponents/ExpanceDate.js
@@ -1,21 +1,17 @@
-import './ExpanceDate.css';
+import "./ExpanceDate.css";
 
 function ExpanceDate(props) {
+  const date = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const month = props.date.toLocaleString("en-US", { month: "long" }).substr(0,3);
+  const year = props.date.getFullYear();
 
-    const date = props.date.toLocaleString('en-US', {day:'2-digit'});
-    const month = props.date.toLocaleString('en-US', {month:'long'});
-
-    const year = props.date.getFullYear();
-
-    return (
-        <div className='expance-date'>
-            <div className='expance-data-month'>{month}</div>
-            <div className='expance-data-year'>{year}</div>
-            <div className='expance-date-date'>{date}</div>
-        </div>
-    )
-
+  return (
+    <div className="expance-date">
+      <div className="expance-data-month">{month}</div>
+      <div className="expance-data-year">{year}</div>
+      <div className="expance-date-date">{date}</div>
+    </div>
+  );
 }
-
 
 export default ExpanceDate;
--- src/components/subComponents/ExpanceDetails.js
+++ src/components/subComponents/ExpanceDetails.js
@@ -1,14 +1,13 @@
-import './ExpanceDetails.css';
+import "./ExpanceDetails.css";
 
 function ExpanceDetails(props) {
-    return (
-        <div className="expance-details">
-            <div className="expance-details-title">{props.title}</div>
-            <div className="expance-details-location">{props.location}</div>
-            <div className="expance-details-amount">Rs{props.amount}</div>
-
-        </div>
-    );
+  return (
+    <div className="expance-details">
+      <div className="expance-details-title">{props.title}</div>
+      <div className="expance-details-location">{props.location}</div>
+      <div className="expance-details-amount">Rs{props.amount}</div>
+    </div>
+  );
 }
 
 export default ExpanceDetails;
