fa490eb034d041f4ab7c2d3f193260f6068d3a1e
vikaskumar0205 <vikaskumar808425@gmail.com>
2023-07-17 11:47:01 +0530

    setting up something

--- src/components/UI/Card.js
+++
@@ -1,8 +0,0 @@
-import './Card.css';
-
-const Card = (props)=>{
-    const newClass = `card ${props.className}`;
-    return <div className={newClass}>{props.children}</div>
-}
-
-export default Card;
